f2ceb3a5bbd960ef7b8208e33ba8a53a2f29dbaa
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-06-04 13:58:30 -0300

    Add files via upload

---
+++ Lista3Ex2/Lista3Ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1,n2;
+            
+                Console.Write("1° valor positivo: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("2° valor positivo: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                        if (n1>n2)
+                        do
+                        {
+                        Console.Write("2° valor positivo: ");
+                        n2 = Convert.ToDouble(Console.ReadLine());
+                        } while(n2<n1);
+            
+               
+
+        
+        }
+    }
+}
