67d0e8d2c5b6bfe04b2e7dd2082faffea6c3e8c0
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-06-04 13:59:13 -0300

    Add files via upload

---
+++ Lista3Ex4/Lista3Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, num;
+            i = 00;
+            num = 00;
+
+
+            do
+            {
+                i = i + 5;
+                num++;
+                Console.WriteLine("5 x {0} = {1}", num, i);
+            }
+
+            while (num < 10);
+        }
+    }
+}
