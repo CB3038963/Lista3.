25e017eaa271fff240bc58c9c3cf4ff2ddc130aa
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-06-04 14:01:04 -0300

    Add files via upload

---
+++ Lista3Ex9/Lista3Ex9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c;
+
+            Console.WriteLine("{0} ", b);
+
+            for (int i = 0; i < 29; i++)
+
+            {
+
+                c = a + b;
+                Console.WriteLine("{0} ", c);
+                a = b;
+                b = c;
+
+            }
+        }
+    }
+}
