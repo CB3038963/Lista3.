8e07d822f1b3114ff9bac849b8fbd9e93ed62015
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-06-04 13:58:48 -0300

    Add files via upload

---
+++ Lista3Ex3/Lista3Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string gen;
+            Console.WriteLine("Genero F mulher , M homem : ");
+            gen = Convert.ToString(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Genero F mulher , M homem : ");
+                gen = Convert.ToString(Console.ReadLine());
+            }
+            while (gen != "F" && gen != "M");
+        }
+    }
+}
