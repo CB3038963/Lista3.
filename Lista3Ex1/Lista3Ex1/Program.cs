3d0458abf2e3b50da23fbc5fd9375c9e002d537e
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-06-04 13:58:12 -0300

    Add files via upload

---
+++ Lista3Ex1/Lista3Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double num;
+
+            do
+            {
+                Console.Write("Digite um valor positivo: ");
+                num = Convert.ToDouble(Console.ReadLine());
+            } while (num < 0);
+        }
+    }
+}
